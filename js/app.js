eaa935355091bc0bec2e06e070e81f5a2827a2d6
Seth Trowbridge <seth111@gmail.com>
2023-05-12 08:56:52 -0400

    remove deno.map.json

--- js/app.js
+++ js/app.js
@@ -5,17 +5,26 @@ import Preact from "preact";
 import * as Store from "./store.js";
 import People from "./people.js";
 
-const Configure = { theme: {}, presets: [TWPreTail(), TWPreAuto()] };
-const ShadowDOM = document.querySelector("#app").attachShadow({ mode: "open" });
-const ShadowDiv = document.createElement("div");
-const ShadowCSS = document.createElement("style");
-ShadowDOM.append(ShadowCSS);
-ShadowDOM.append(ShadowDiv);
+/** @type {TW.TwindUserConfig} */
+const Configure = {
+  theme: {},
+  presets:
+    /** @type {TW.Preset<TW.BaseTheme>[]} */ ([TWPreTail(), TWPreAuto()]),
+};
 
-TW.observe(TW.twind(Configure, TW.cssom(ShadowCSS)), ShadowDiv);
-Preact.render(
-  Preact.createElement(Store.Provider, {
-    children: Preact.createElement(People, null),
-  }),
-  ShadowDiv,
-);
+const AtElement = document.querySelector("#app");
+if (AtElement) {
+  const ShadowDOM = AtElement.attachShadow({ mode: "open" });
+  const ShadowDiv = document.createElement("div");
+  const ShadowCSS = document.createElement("style");
+  ShadowDOM.append(ShadowCSS);
+  ShadowDOM.append(ShadowDiv);
+
+  TW.observe(TW.twind(Configure, TW.cssom(ShadowCSS)), ShadowDiv);
+  Preact.render(
+    Preact.createElement(Store.Provider, {
+      children: Preact.createElement(People, null),
+    }),
+    ShadowDiv,
+  );
+}
